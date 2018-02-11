cea9857aa9c5334953dc61c22e81696cffc90394
VerasUnB <thiago.wortnnes@gmail.com>
2018-02-11 23:55:18 +0000

    List of Clients and redirect to idividual interview

--- php/readDB.php
+++ php/readDB.php
@@ -1,36 +1,3 @@
 <?php
- 
-require("conection.php");
 
-function parseToXML($htmlStr)
-{
-$xmlStr=str_replace('<','&lt;',$htmlStr);
-$xmlStr=str_replace('>','&gt;',$xmlStr);
-$xmlStr=str_replace('"','&quot;',$xmlStr);
-$xmlStr=str_replace("'",'&#39;',$xmlStr);
-$xmlStr=str_replace("&",'&amp;',$xmlStr);
-return $xmlStr;
-}
-
-// Select all the rows in the markers table
-$query = "SELECT * FROM dadoscliente";
-$result = mysqli_query($conn, $query);
-if (!$result) {
-  die('Invalid query: ' . mysql_error());
-}
-
-header("Content-type: text/xml");
-
-// Start XML file, echo parent node
-echo '<markers>';
-
-// Iterate through the rows, printing XML nodes for each
-while ($row = mysqli_fetch_assoc($result)){
-  // Add to XML document node
-  echo '<marker ';
-  echo 'name="' . parseToXML($row['nome']) . '" ';
-  echo '/>';
-}
-
-// End XML file
-echo '</markers>';
+echo "teste";
